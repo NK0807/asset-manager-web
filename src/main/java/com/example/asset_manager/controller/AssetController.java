8c4f4e5541ab3c55613137417d40dc728c5ee984
NK0807 <hyue3434@gmail.com>
2026-01-28 02:47:17 +0900

    入力チェック機能(バリデーション)を追加

--- src/main/java/com/example/asset_manager/controller/AssetController.java
+++ src/main/java/com/example/asset_manager/controller/AssetController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -28,12 +30,29 @@ public class AssetController {
 		// 画面に渡す
 		model.addAttribute("assets", assetList);
 		model.addAttribute("total", total);
+		
+		// 空っぽのフォーム用データ
+		if(!model.containsAttribute("asset")) {
+			model.addAttribute("asset", new Asset());
+		}
 		return "index";
 	}
 	
 	// データ登録
 	@PostMapping("/add")
-	public String add(Asset asset) {
+	public String add(@Validated Asset asset, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			// 失敗したので保存しない
+			
+			// 画面下の一覧表示用データだけ再取得して渡す（これがないと画面が崩れる）
+			List<Asset> assetList = service.findAll();
+			model.addAttribute("assets", assetList);
+			model.addAttribute("total", service.getTotalAmount());
+			
+			return "index";
+		}
+		
+		// エラーがなければ保存して再読み込み
 		service.save(asset);
 		return "redirect:/";
 	}
